53ed850b759251783bb04d3f7b1cf7c4c51d2806
Stryukov <stryukov.semen@gmail.com>
2023-11-14 23:44:45 +1200

    Update send_telegram_message.py

--- send_telegram_message.py
+++ send_telegram_message.py
@@ -1,27 +1,41 @@
 import requests
-import sys
+import argparse
 
 
-def send_telegram_message(*args):
-    message = f"{args[3]} created commit:\n" \
-              f"Commit message: {args[4]}\n" \
-              f"Repository: {args[5]}\n" \
-              f"See changes: {args[6]}\n" \
+def send_telegram_message(
+        bot_token,
+        chat_id,
+        message_thread_id,
+        actor,
+        message,
+        repository,
+        commit_url,
+        pr_title,
+        pr_number,
+        pr_author,
+        pr_base_branch,
+        pr_head_branch,
+        pr_url,
+):
+    message = f"{actor} created commit:\n" \
+              f"Commit message: {message}\n" \
+              f"Repository: {repository}\n" \
+              f"See changes: {commit_url}\n" \
               f"\n👌 Deployment successfully completed!\n" \
               f"\nPR Information:" \
-              f"\nTitle: {args[7]}" \
-              f"\nPR Number: {args[8]}" \
-              f"\nAuthor: {args[9]}" \
-              f"\nBase Branch: {args[10]}" \
-              f"\nHead Branch: {args[11]}" \
-              f"\nURL: {args[12]}"
+              f"\nTitle: {pr_title}" \
+              f"\nPR Number: {pr_number}" \
+              f"\nAuthor: {pr_author}" \
+              f"\nBase Branch: {pr_base_branch}" \
+              f"\nHead Branch: {pr_head_branch}" \
+              f"\nURL: {pr_url}"
 
-    url = f'https://api.telegram.org/bot{args[0]}/sendMessage'
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
     params = {
-        'chat_id': args[1],
+        'chat_id': chat_id,
         'text': message,
         'parse_mode': 'HTML',
-        'message_thread_id': args[2]
+        'message_thread_id': message_thread_id
     }
 
     response = requests.post(url, params=params)
@@ -34,5 +48,26 @@ def send_telegram_message(*args):
 
 
 if __name__ == '__main__':
-    print(*sys.argv)
-    send_telegram_message(*sys.argv)
+    parser = argparse.ArgumentParser(description='Send a Telegram message.')
+    parser.add_argument('--bot_token', required=True)
+    parser.add_argument('--chat_id', required=True)
+    parser.add_argument('--message_thread_id')
+    parser.add_argument('--actor', required=True)
+    parser.add_argument('--message')
+    parser.add_argument('--repository')
+    parser.add_argument('--commit_url')
+    parser.add_argument('--pr_title')
+    parser.add_argument('--pr_number')
+    parser.add_argument('--pr_author')
+    parser.add_argument('--pr_base_branch')
+    parser.add_argument('--pr_head_branch')
+    parser.add_argument('--pr_url')
+
+    args = parser.parse_args()
+
+    send_telegram_message(
+        args.bot_token, args.chat_id, args.message_thread_id, args.actor,
+        args.message, args.repository, args.commit_url, args.pr_title,
+        args.pr_number, args.pr_author, args.pr_base_branch,
+        args.pr_head_branch, args.pr_url
+    )
