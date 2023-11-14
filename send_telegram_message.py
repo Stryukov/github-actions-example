fe9c404498915b5a2e25f873240f570d07b20395
Stryukov <stryukov.semen@gmail.com>
2023-11-14 23:09:56 +1200

    Update send_telegram_message.py

--- send_telegram_message.py
+++ send_telegram_message.py
@@ -1,15 +1,37 @@
-import os
+import requests
 import sys
-from aiogram import Bot, types
 
-def send_telegram_message(bot_token, chat_id):
-    bot = Bot(token=bot_token)
-    message_text = 'Привет, группа! Это сообщение от вашего бота из Github actions.'
 
-    bot.send_message(chat_id, message_text, parse_mode=types.ParseMode.HTML, thread_id=9)
+def send_telegram_message(*args):
+    message = f"{args[3]} created commit:\n" \
+              f"Commit message: {args[4]}\n" \
+              f"Repository: {args[5]}\n" \
+              f"See changes: {args[6]}\n" \
+              f"\n👌 Deployment successfully completed!\n" \
+              f"\nPR Information:" \
+              f"\nTitle: {args[7]}" \
+              f"\nPR Number: {args[8]}" \
+              f"\nAuthor: {args[9]}" \
+              f"\nBase Branch: {args[10]}" \
+              f"\nHead Branch: {args[11]}" \
+              f"\nURL: {args[12]}"
+
+    url = f'https://api.telegram.org/bot{args[0]}/sendMessage'
+    params = {
+        'chat_id': args[1],
+        'text': message,
+        'parse_mode': 'HTML',
+        'message_thread_id': args[2]
+    }
+
+    response = requests.post(url, params=params)
+
+    if response.status_code == 200:
+        print("Message sent successfully!")
+    else:
+        print(f"Failed to send message. Status code: {response.status_code}")
+        print(response.text)
+
 
 if __name__ == '__main__':
-    bot_token = sys.argv[1]
-    chat_id = sys.argv[2]
-
-    send_telegram_message(bot_token, chat_id)
+    send_telegram_message(*sys.argv)
